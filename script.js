36fc21cfbbea8f4329bd1dbe9fd2d96c8dbb4a86
n242004 <nishnat.chauhan_cs.aiml21@gla.ac.in>
2023-12-14 04:53:46 -0400

    fourth commit

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const getCityCoordinates = () => {
     const API_URL = https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY};
   
     fetch(API_URL).then(response => response.json()).then(data => {
-        if (!data.length) return alert(No coordinates found for ${cityName});
+        if (!data.length) return alert('No coordinates found for ${cityName}');
         const { lat, lon, name } = data[0];
         getWeatherDetails(name, lat, lon);
     }).catch(() => {
